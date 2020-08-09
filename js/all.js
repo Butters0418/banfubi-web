e5e9afa90e6aaf2d7967a0418734e23697605d11
Butters <36911374+Butters0418@users.noreply.github.com>
2020-08-09 22:51:38 +0800

    story

--- js/all.js
+++ js/all.js
@@ -1,5 +1,6 @@
 $(function () {
-  var imgs = document.images,
+  // img loading
+  let imgs = document.images,
     len = imgs.length,
     counter = 0;
   [].forEach.call(imgs, function (img) {
@@ -14,7 +15,7 @@ $(function () {
       console.log('All img load')
     }
   }
-  var loadingtime = 1000;
+  let loadingtime = 1000;
   function countDown() {
     console.log(loadingtime)
     loadingtime -= 200
@@ -23,19 +24,17 @@ $(function () {
       $('.loading_mask').fadeOut(300);
     }
   }
-  var timer = setInterval(countDown, 200);
+  let timer = setInterval(countDown, 200);
 
-
-
-
-
+  // [首頁] blog text
   let textLength = $(window).width() > 768 ? 155 : 110;
   $('.blog__footer p').each(function (item, index) {
     let newText = $(this).text().slice(0, textLength) + '...';
     $(this).html(newText);
   })
-  //banner 輪播
-  var websiteBannerSwiper = new Swiper('.website__banner .swiper-container', {
+
+  // [首頁] banner 輪播
+  let websiteBannerSwiper = new Swiper('.website__banner .swiper-container', {
     spaceBetween: 0,
     loop: true,
     autoplay: true,
@@ -49,8 +48,8 @@ $(function () {
     },
   });
 
-  // pd-box1
-  var pd1Swiper = new Swiper('#pd-box1 .swiper-container', {
+  // [首頁] pd-box1
+  let pd1Swiper = new Swiper('#pd-box1 .swiper-container', {
     slidesPerView: 4,
     spaceBetween: 40,
     simulateTouch: false,
@@ -66,8 +65,8 @@ $(function () {
       }
     }
   });
-  // pd-box2
-  var pd2Swiper = new Swiper('#pd-box2 .swiper-container', {
+  // [首頁] pd-box2
+  let pd2Swiper = new Swiper('#pd-box2 .swiper-container', {
     slidesPerView: 4,
     spaceBetween: 40,
     simulateTouch: false,
@@ -83,8 +82,8 @@ $(function () {
       }
     }
   });
-  // pd-box3
-  var pd3Swiper = new Swiper('#pd-box3 .swiper-container', {
+  // [首頁] pd-box3
+  let pd3Swiper = new Swiper('#pd-box3 .swiper-container', {
     slidesPerView: 4,
     spaceBetween: 40,
     simulateTouch: false,
@@ -100,8 +99,8 @@ $(function () {
       }
     }
   });
-  // blog
-  var blogSlider = new Swiper('.blog__slider .swiper-container', {
+  // [首頁] blog 輪播
+  let blogSlider = new Swiper('.blog__slider .swiper-container', {
     slidesPerView: 1,
     pagination: {
       clickable: true,
@@ -112,6 +111,82 @@ $(function () {
       prevEl: '.blog__slider .swiper-prev',
     },
   });
+
+  // [品牌故事 story] 
+  let rate = $(window).width() > 768 ? 1 : 0.6;
+  window.sr = ScrollReveal({ viewOffset: { bottom: 150 * rate }, });
+  // [品牌故事 story] 自定義動畫集合
+  sr.reveal('.story--fadeup', { origin: 'bottom', distance: '10px', duration: 500, delay: 100, reset: true, beforeReveal: countData });
+  sr.reveal('.story--ani', { origin: 'bottom', distance: 0, afterReveal: storyAni })
+  let storyData = [
+    {
+      val: 7,
+      unit: 1
+    },
+    {
+      val: 500,
+      unit: 25,
+    },
+    {
+      val: 35000,
+      unit: 1400,
+    }
+  ]
+  //[品牌故事 story] 計數器
+  function countData() {
+    $('.val').each(function (index) {
+      [val, unit, item] = [storyData[index].val, storyData[index].unit, $(this)]
+      startCount(val, unit, item);
+    })
+  }
+  function startCount(val, unit, item) {
+    let time = 0;
+    function countDown() {
+      item.text(time)
+
+      if (time === val) {
+        clearInterval(timer)
+      }
+      time += unit;
+    }
+    let timer = setInterval(countDown, 60);
+  }
+  //[品牌故事 story] 動畫
+  let $storyLogo = $('.website__story--ani .logo'),
+    $storyCircle = $('.website__story--ani .circle'),
+    $text = $('.website__story--ani .text'),
+    $triangle = $('.website__story--ani .triangle'),
+    $slogan = $('.website__story--slogan p')
+  function storyAni() {
+    let tl = gsap.timeline();
+    tl.to($storyCircle, { duration: 0.5, scale: 1 })
+      .to($storyLogo, { duration: 0.5, opacity: 1, onComplete: startTriangleAni }, 0.4)
+      .to($text, { duration: 0.5, opacity: 1, y: 0 }, 1.8)
+      .to($slogan, { duration: 0.5, y: 0, opacity: 1 }, 1.8)
+  }
+  function startTriangleAni() {
+    $triangle.addClass('triangle-ani')
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // scroll
   $('a[href^="#"]').on('click', function (e) {
     e.preventDefault();
@@ -121,6 +196,11 @@ $(function () {
       scrollTop: targetTop
     }, 300);
   });
+
+
+
+
+
 });
 
 

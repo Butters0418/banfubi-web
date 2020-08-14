89c30eb00e0db0c321eb310410a247cb0aa0f2ae
Butters <swki418@gmail.com>
2020-08-14 18:37:25 +0800

    qa.html

--- js/qa.js
+++ js/qa.js
@@ -1,6 +1,21 @@
 $(function () {
-  $('.website__qa--question h3').on('click', function () {
-    let $nextDiv = $(this).next('div');
-    $nextDiv.toggleClass('show');
+
+  let activeTag = 'order';
+  $(".website__qa--question:not([data-value='order'])").addClass('d-none')
+
+  $('.website__qa--tag').on('click', function (e) {
+    e.preventDefault();
+    $(this).addClass('active').siblings().removeClass('active');
+    activeTag = $(this).data('tag');
+
+    let currentQuestions = $('.website__qa--question').filter(function () {
+      return $(this).data('value') === activeTag;
+    })
+
+    $('.website__qa--question').addClass('d-none').find('.collapse').removeClass('show')
+    currentQuestions.each(function () {
+      $(this).removeClass('d-none').addClass('fadeInLeft')
+    })
+
   })
 })

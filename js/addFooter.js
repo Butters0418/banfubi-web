41ee1ca1b8bead90ce421b040ad7e99e85efb731
Butters <36911374+Butters0418@users.noreply.github.com>
2020-09-03 22:04:41 +0800

    footer

--- js/addFooter.js
+++ js/addFooter.js
@@ -2,41 +2,66 @@ $(function () {
   $('.page-content').append('<section class="code" id="website__footer"></section>')
   let footerHtml = `<section class="website__section section5">
       <div class="section__inner">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-3 col-sm-5 col-md-4">
-              <div class="footer__logo">
-                <img src="https://butters0418.github.io/banfubi-web/img/logo2.svg" alt="banfubi">
-              </div>
-            </div>
-            <div class="col-md-2 pc">
-              <p class="footer__title">關於我們</p>
-              <ul class="footer__list">
-                <li><a href="https://www.banfubi.com.tw/brandstory">品牌故事</a></li>
-                <li><a href="">FB粉絲專頁 </a></li>
-                <li><a href="https://www.banfubi.com.tw/ru9l56">實體門市 </a></li>
-                <li><a href="https://www.banfubi.com.tw/pm6j8d">隱私權政策</a></li>
-              </ul>
-            </div>
-            <div class="col-md-2 pc">
-              <p class="footer__title">常見Q&A</p>
-              <ul class="footer__list qalist">
-                <li><a href="https://www.banfubi.com.tw/x7uc4m#pay">付款方式</a></li>
-                <li><a href="https://www.banfubi.com.tw/x7uc4m#shipping">出貨配送 </a></li>
-                <li><a href="https://www.banfubi.com.tw/x7uc4m#try">七天鑑賞期</a></li>
-                <li><a href="https://www.banfubi.com.tw/x7uc4m#invoice">電子發票相關</a></li>
-              </ul>
-            </div>
-            <div class="col-9 col-sm-7 col-md-4 text-md-center">
-              <p class="footer__title pc">聯繫我們</p>
-              <ul class="footer__list">
-                <li>客服電話:(02)2581-9665</li>
-                <li>服務時間:週一至週五 10:00-18:00 </li>
-                <li>廠商合作:lulu@banfubi.com</li>
-              </ul>
-            </div>
+        <!-- pc footer logo  -->
+        <div class="footer__logo pc">
+          <img src="https://butters0418.github.io/banfubi-web/img/logo2.svg" alt="banfubi">
+        </div>
+        <!-- mb icon -->
+        <div class="footer__social mb">
+          <a href=""><img src="https://butters0418.github.io/banfubi-web/img/fb.svg"></a>
+          <a href=""><img src="https://butters0418.github.io/banfubi-web/img/line.svg"></a>
+          <a href=""><img src="https://butters0418.github.io/banfubi-web/img/ig.svg"></a>
+        </div>
+
+        <div class="footer__collapse footer__aboutus">
+          <p class="footer__title" data-toggle="collapse" href="#footer-1">ABOUT US <span class="mb footer__title--arrow"></span></p>
+          <div class="collapse show" id="footer-1">
+            <ul class=" footer__list">
+              <li><a href="https://www.banfubi.com.tw/brandstory">品牌故事</a></li>
+              <li><a href="">Facebook粉絲團 </a></li>
+              <li><a href="https://www.banfubi.com.tw/ru9l56">myheart實體門市 </a></li>
+              <li><a href="https://www.banfubi.com.tw/pm6j8d">隱私權政策</a></li>
+            </ul>
           </div>
         </div>
+
+        <div class="footer__collapse footer__qa">
+          <p class="footer__title" data-toggle="collapse" href="#footer-2">Q&A <span class="mb footer__title--arrow"></span></p>
+          <div class="collapse show" id="footer-2">
+            <ul class="footer__list">
+              <li><a href="https://www.banfubi.com.tw/x7uc4m#pay">付款方式</a></li>
+              <li><a href="https://www.banfubi.com.tw/x7uc4m#shipping">出貨配送 </a></li>
+              <li><a href="https://www.banfubi.com.tw/x7uc4m#try">七天鑑賞期</a></li>
+              <li><a href="https://www.banfubi.com.tw/x7uc4m#invoice">電子發票相關</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="footer__collapse footer__contactus">
+          <p class="footer__title" data-toggle="collapse" href="#footer-3">CONTACT US <span class="mb footer__title--arrow"></span></p>
+          <div class="collapse show" id="footer-3">
+            <ul class="footer__list">
+              <li>斑富比有限公司</li>
+              <li>客服電話:(02)2581-9665</li>
+              <li>服務時間:週一至週五 10:00-18:00 </li>
+              <li>廠商合作:lulu@banfubi.com</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="footer__pay mb">
+          <a href=""><img src="https://butters0418.github.io/banfubi-web/img/visa.svg"></a>
+          <a href=""><img src="https://butters0418.github.io/banfubi-web/img/mastercard.svg"></a>
+          <a href=""><img src="https://butters0418.github.io/banfubi-web/img/linepay.png"></a>
+          <a href=""><img src="https://butters0418.github.io/banfubi-web/img/newebpay.png"></a>
+          <a href=""><img src="https://butters0418.github.io/banfubi-web/img/ssl.png"></a>
+        </div>
+
+        <div class="footer__copyright mb">
+          <img src="https://butters0418.github.io/banfubi-web/img/logo-white.png">
+          <p>©2020 Banfubi Inc. All right reserved.</p>
+        </div>
+
       </div>
     </section>`
   $('#website__footer').html(footerHtml);
---
+++ js/pdData.js
@@ -0,0 +1,14 @@
+let pdData = [
+  {
+    name: 'test1',
+    price: '$100'
+  },
+  {
+    name: 'test2',
+    price: '$100'
+  },
+  {
+    name: 'test3',
+    price: '$100'
+  },
+]
